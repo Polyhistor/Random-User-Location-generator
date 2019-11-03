46c5932b6df42274082f3d6010928af9cee76aae
Pouya Ataei <pouya.ataei.7@gmail.com>
2019-11-03 20:42:05 +1300

    finalized classes

--- classes.ts
+++ classes.ts
@@ -1,10 +1,19 @@
 class Vehicle {
+  constructor(public color: string) {}
+
   protected honk(): void {
     console.log("beep");
   }
 }
 
+const vehicle = new Vehicle("orange");
+console.log(vehicle.color);
+
 class Car extends Vehicle {
+  constructor(public wheels: number, color: string) {
+    super(color);
+  }
+
   private drive() {
     console.log("vroom");
   }
@@ -15,5 +24,5 @@ class Car extends Vehicle {
   }
 }
 
-const carNew = new Car();
+const carNew = new Car(4, "red");
 carNew.startDrivingProcess();
